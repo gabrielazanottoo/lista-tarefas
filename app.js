503c3f409936dc71cf32703b12ddffefc7d841f9
gabrielazanottoo <gabrielazanotto43@gmail.com>
2026-05-26 09:29:59 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const button = document.getElementById('button')
 
 function inserirTarefa(){
     let tarefa =  input.value
+
+    if(tarefa == ''){
+        alert('Por favor, insira uma tarefa')
+        return
+    }
+
     display.innerHTML += 
     `<div class="tarefa">
     ${tarefa} 
@@ -16,9 +22,11 @@ function inserirTarefa(){
     input.value = ''
 }
 
-if(input.value != ''){
-    alert('Por favor, insira uma tarefa')
-}else{
-    button.addEventListener("click", inserirTarefa)
+function deletarTarefa(event){
+    if(event.target.classList.contains("delete")){
+    event.target.parentElement.parentElement.remove()
+    }
 }
+button.addEventListener("click", inserirTarefa)
+display.addEventListener("click", deletarTarefa)
 
